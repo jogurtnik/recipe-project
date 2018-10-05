71849e354e7802efe27dc767c3a7788cf8f70c53
Remi <remigiusz.widz@gmail.com>
2018-10-05 20:34:01 +0100

    Completing assignment

--- src/main/java/uk/co/punishell/recipeproject/domain/Ingredient.java
+++ src/main/java/uk/co/punishell/recipeproject/domain/Ingredient.java
@@ -12,6 +12,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -37,6 +40,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 
     public Recipe getRecipe() {
